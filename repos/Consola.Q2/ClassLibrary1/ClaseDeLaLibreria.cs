91b40e499411f2a2515035ded3b6a18a83154fa6
unknown <eugeperonace@gmail.com>
2020-09-23 01:40:45 -0300

    Clase1!

---
+++ repos/Consola.Q2/ClassLibrary1/ClaseDeLaLibreria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ClaseDeLaLibreria
+    {
+        public static void Metodo()
+        { 
+
+        }
+    }
+}
---
+++ repos/Consola.Q2/Consola.Q2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClassLibrary;
+
+namespace Consola.Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.Write("Hola");
+            Console.WriteLine("Esto trae un salto de línea");
+            Console.WriteLine(i);
+            Console.ReadKey();
+
+            ClaseDeLaLibreria variable = new ClaseDeLaLibreria();
+        }
+    }
+}
